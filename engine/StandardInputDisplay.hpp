5cedd066b6ef2f798b10d5d479ed98793d340d76
Kacu <k.ptychu@gmail.com>
2020-12-17 15:59:32 +0100

    code cleanup: StandardInputDisplay.hpp

--- engine/StandardInputDisplay.hpp
+++ engine/StandardInputDisplay.hpp
@@ -17,40 +17,10 @@ public:
         keypad(window_, true);  
     }
 
-
-
     // `DisplayInterface` OPERATIONS:
     void redraw() override {
         // wclear(window_);
         // wresize(window_,scrheight_, scrwidth_);
         keypad(window_, true);
     }
-    // void update(int highlight) { update(static_cast<size_t>(highlight)); }
-    // void update(size_t highlight) { highlight_ = highlight; }
-
-
-    // size_t get_Size() const { return choices_.size(); }
-    // size_t get_Index() const { return highlight_; }
-    // std::string get_Value() const { return choices_[highlight_]; }
-
-    /* 
-    size_t operator ++() {
-        ++highlight_;
-
-        if (highlight_ >= choices_.size()) {
-            highlight_ = 0;
-        }
-        return highlight_;
-    }
-
-    size_t operator --() {
-        --highlight_;
-
-        if (highlight_ >= choices_.size()) {
-            highlight_ = choices_.size() - 1;
-        }
-        return highlight_;
-    }
-    // */
-
 };

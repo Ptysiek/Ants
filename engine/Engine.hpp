a91339a9434841f8a29f2e28e376489fe83099a2
Kacu <k.ptychu@gmail.com>
2020-12-17 15:39:33 +0100

    new class: Engine, new file: Engine.hpp

---
+++ engine/Engine.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <curses.h>
+#include <memory>
+#include <stack>
+
+#include "i_Scene.hpp"
+
+
+class Engine {
+
+protected:
+    std::stack<std::shared_ptr<i_Scene>> scenes_;
+
+
+public:
+    virtual void initCurses() final {
+        
+        //resizeterm(50, 300);
+        //wresize(stdscr, 50, 300);
+
+        // [cbreak() / raw()] / [halfdelay(int) / nodelay(win, bool) / timeout(int)]
+        cbreak();
+        // raw();   
+        // halfdelay(1);
+        
+        // keypad(stdscr, TRUE);
+
+        // [echo() / noecho()]
+        noecho();       
+        curs_set(0);
+
+        start_color();  
+
+    }
+
+    virtual void initStage(std::shared_ptr<i_Scene> scene) {
+        scenes_.push(scene);
+        gameLoop();
+    }
+
+
+protected:
+    virtual void gameLoop() {
+        if (scenes_.empty()) {
+            return;
+        }
+
+        clear();
+        auto& scene = scenes_.top();
+
+        scene->init();
+        scene->render();
+
+        while(!scene->newcall()) {
+            if (scene->input()) {
+                scene->render();
+            }
+        }
+
+        CallProcessing(scene->get_Call());
+    }
+
+    virtual void CallProcessing(std::string call) final {
+        if (call.empty()) {
+        }
+        else if (call == "StackRemove" || call == "Exit") {
+            scenes_.pop();
+        }
+
+
+        // gameLoop();
+    }
+
+
+
+};
